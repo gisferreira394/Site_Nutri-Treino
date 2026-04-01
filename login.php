ff06c463a1e5752ae1523e63777a132f32d0ca4c
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2026-04-01 15:50:51 -0300

    login.php

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nutri&Treino</title>
+</head>
+<body>
+    <form class="login" action="validar.php" method="POST">
+        <h1>Login</h1>
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="senha" placeholder="Senha" required>
+    </form>
+</body>
+</html>
