0703ece6156d00c3781a52c1d7b2690e8fc40290
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2026-04-02 14:33:46 -0300

    estilo_login

--- login.php
+++ login.php
@@ -4,12 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nutri&Treino</title>
+    <link rel="stylesheet" href="stylelogin.css
+	">
 </head>
 <body>
+
+<div class="container">
     <form class="login" action="validar.php" method="POST">
+        
+        <div class="icon">👤</div>
         <h1>Login</h1>
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="senha" placeholder="Senha" required>
+
+        <div class="input-box">
+            <input type="email" name="email" required>
+            <label>Email</label>
+        </div>
+
+        <div class="input-box">
+            <input type="password" name="senha" required>
+            <label>Senha</label>
+        </div>
+
+        <div class="options">
+            <a href="#">Esqueceu a senha?</a>
+        </div>
+
+        <button type="submit">LOGIN</button>
+
     </form>
+</div>
+
+
 </body>
 </html>
