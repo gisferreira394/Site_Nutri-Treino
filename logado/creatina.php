dd50e87489519ffc3fe6f70698a1a283ccd37140
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2026-04-29 18:24:17 -0300

    .

--- logado/creatina.php
+++ logado/creatina.php
@@ -16,15 +16,15 @@ $dados = mysqli_fetch_assoc($res);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Creatina</title>
-    <link rel="stylesheet" href="../css/styleswhey.css">
+    <link rel="stylesheet" href="css/styleswhey.css">
 </head>
 <body>
 
    <header class="site-header">
     <div class="topbar container">
      
-       <a href="usuariologado.php" class="logo-link">
-    <img src="../img/logonutri.png" alt="Logo" class="logo">
+       <a href="index.html" class="logo-link">
+    <img src="img/logonutri.png" alt="Logo" class="logo">
 </a>
 
       <form class="search" onsubmit="return buscarProduto(event)">
@@ -98,11 +98,11 @@ $dados = mysqli_fetch_assoc($res);
 <script>
   // produtos simulados
   const produtos = [
-    { nome: "Whey Protein", url: "whey.php" },
-    { nome: "Creatina", url: "creatina.php" },
-    { nome: "vitaminas", url: "vitaminas.php" },
-    { nome: "Pré-treino", url: "pretreino.php" },
-    { nome: "barrinhas", url: "barrinhas.php" }
+    { nome: "Whey Protein", url: "whey.html" },
+    { nome: "Creatina", url: "creatina.html" },
+    { nome: "vitaminas", url: "vitaminas.html" },
+    { nome: "Pré-treino", url: "pretreino.html" },
+    { nome: "barrinhas", url: "barrinhas.html" }
   ];
 
   const campoBusca = document.getElementById("campoBusca");
@@ -140,16 +140,17 @@ $dados = mysqli_fetch_assoc($res);
       <nav class="quick-icons" aria-label="Acesso rápido">
         
           <!-- ícone usuário -->
-          <a href="#" onclick="abrirPainel()"> 
+          <a href="login.php"> 
           <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
             <path d="M20 21a8 8 0 0 0-16 0"></path>
             <circle cx="12" cy="7" r="4"></circle>
           </svg>
+          <span>Minha conta</span>
         </a>
         
-  <!-- ícone sacola -->
-          <a href="../carrinho.php" class="link-cesta" aria-label="Carrinho de compras">
+         <!-- ícone sacola -->
+          <a href="carrinho.html" class="link-cesta" aria-label="Carrinho de compras">
   <svg
     class="icone-cesta"
     width="22"
@@ -166,10 +167,11 @@ $dados = mysqli_fetch_assoc($res);
     <path d="M9 10v10a3 3 0 0 0 3 3 3 3 0 0 0 3-3V10"></path>
   </svg>
 </a>
+</a>
+        </a>
     
           <!-- ícone coração -->
- <a href="../favoritos.html" class="link-favorito" aria-label="favoritos">
-
+           <a href="favoritos.html" class="link-favorito" aria-label="favoritos">
           <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
             <path d="M20.8 4.6a5.5 5.5 0 0 0-7.8 0L12 5.6l-1-1a5.5 5.5 0 0 0-7.8 7.8l1 1L12 22l7.8-8.6 1-1a5.5 5.5 0 0 0 0-7.8Z"></path>
@@ -182,11 +184,11 @@ $dados = mysqli_fetch_assoc($res);
     
   <nav class="main-nav" aria-label="Principal">
         <ul class="nav-list">
-            <li class="nav-item"><a class="nav-link" href="../consultas.php">Consultas</a></li>
-            <li class="nav-item"><a class="nav-link"href="treinos.php">Treinos</a></li>
-            <li class="nav-item"><a class="nav-link" href="produto.php">Produtos</a></li>
-            <li class="nav-item"><a class="nav-link" href="gympass.php">Buscar Academias</a></li>
-            <li class="nav-item"><a class="nav-link" href="planos.php">Planos</a></li>
+            <li class="nav-item"><a class="nav-link" href="consultas.php">Consultas</a></li>
+            <li class="nav-item"><a class="nav-link"href="treinos.html">Treinos</a></li>
+            <li class="nav-item"><a class="nav-link" href="produto.html">Produtos</a></li>
+            <li class="nav-item"><a class="nav-link" href="gympass.html">Buscar Academias</a></li>
+            <li class="nav-item"><a class="nav-link" href="planos.html">Planos</a></li>
         </ul>
     </nav>
 
@@ -196,11 +198,11 @@ $dados = mysqli_fetch_assoc($res);
 
     <nav class="categoria">
        <ul class="nav-lists">
-            <li class="nav-item"><a class="nav-link" href="whey.php">Whey</a></li>
-            <li class="nav-item"><a class="nav-link"href="pretreino.php">Pré-Treinos</a></li>
-            <li class="nav-item"><a class="nav-link" href="barrinhas.php">Barrinhas</a></li>
-            <li class="nav-item"><a class="nav-link" href="vitaminas.php">Vitaminas</a></li>
-            <li class="nav-item"><a class="nav-link" href="creatina.php">Creatina</a></li>
+            <li class="nav-item"><a class="nav-link" href="whey.html">Whey</a></li>
+            <li class="nav-item"><a class="nav-link"href="pretreino.html">Pré-Treinos</a></li>
+            <li class="nav-item"><a class="nav-link" href="barrinhas.html">Barrinhas</a></li>
+            <li class="nav-item"><a class="nav-link" href="vitaminas.html">Vitaminas</a></li>
+            <li class="nav-item"><a class="nav-link" href="creatina.html">Creatina</a></li>
         </ul>
     </nav>
  
@@ -215,12 +217,12 @@ $dados = mysqli_fetch_assoc($res);
 
         <!-- Produto 1 -->
        <div class="card">
-            <img src="../imgcreatina/creatina500g.jpeg">  
+            <img src="imgcreatina/creatina500g.jpeg">  
              <p class="titulo">Creatina 500g</p>
-            <p class="preco verde">R$ 94,90 no PIX</p>
+            <p class="preco verde">R$ 89,90 no PIX</p>
             <button>
                <a class="btn-comprar"
-       href="../paginadoproduto.html?nome=Creatina 500g&preco=94.90&img=imgcreatina/creatina500g.jpeg&desc=Creatina 500g">
+       href="paginadoproduto.html?nome=Creatina 500g&preco=89,90&img=imgcreatina/creatina500g.jpeg&desc=Creatina 500g">
         Comprar
     </a>
             </button>
@@ -228,12 +230,12 @@ $dados = mysqli_fetch_assoc($res);
 
         <!-- Produto 2 -->
         <div class="card">
-            <img src="../imgcreatina/creatina250g.jpeg">  
+            <img src="imgcreatina/creatina250g.jpeg">  
              <p class="titulo">Creatina 250g</p>
-            <p class="preco verde">R$ 94,90 no PIX</p>
+            <p class="preco verde">R$ 44,95 no PIX</p>
             <button>
                <a class="btn-comprar"
-       href="../paginadoproduto.html?nome=Creatina 250g&preco=94.90&img=imgcreatina/creatina250g.jpeg&desc=Creatina 250g">
+       href="paginadoproduto.html?nome=Creatina 250g&preco=44,95&img=imgcreatina/creatina250g.jpeg&desc=Creatina 250g">
         Comprar
     </a>
             </button>
@@ -242,12 +244,12 @@ $dados = mysqli_fetch_assoc($res);
 
         <!-- Produto 3 -->
        <div class="card">
-            <img src="../imgcreatina/creapure500g.jpeg">  
+            <img src="imgcreatina/creapure500g.jpeg">  
              <p class="titulo">Creatina creapure 500g</p>
-            <p class="preco verde">R$ 94,90 no PIX</p>
+            <p class="preco verde">R$ 99,90 no PIX</p>
            <button>
                <a class="btn-comprar"
-       href="../paginadoproduto.html?nome=Creatina creapure 500g&preco=94.90&img=imgcreatina/creapure500g.jpeg&desc=Creatina creapure 500g">
+       href="paginadoproduto.html?nome=Creatina creapure 500g&preco=99,90&img=imgcreatina/creapure500g.jpeg&desc=Creatina creapure 500g">
         Comprar
     </a>
             </button>
@@ -255,12 +257,12 @@ $dados = mysqli_fetch_assoc($res);
 
         <!-- Produto 4 -->
         <div class="card">
-            <img src="../imgcreatina/creapure250g.jpeg">  
+            <img src="imgcreatina/creapure250g.jpeg">  
              <p class="titulo">Creatina creapure 250g</p>
-            <p class="preco verde">R$ 94,90 no PIX</p>
+            <p class="preco verde">R$ 49,95 no PIX</p>
             <button>
                <a class="btn-comprar"
-       href="../paginadoproduto.html?nome=Creatina creapure 250g&preco=94.90&img=imgcreatina/creapure250g.jpeg&desc=Creatina creapure 250g">
+       href="paginadoproduto.html?nome=Creatina creapure 250g&preco=49,95&img=imgcreatina/creapure250g.jpeg&desc=Creatina creapure 250g">
         Comprar
     </a>
             </button>
@@ -268,12 +270,12 @@ $dados = mysqli_fetch_assoc($res);
 
         <!-- Produto 5 -->
         <div class="card">
-            <img src="../imgcreatina/creapure1kg.jpeg">  
+            <img src="imgcreatina/creapure1kg.jpeg">  
              <p class="titulo">Creatina creapure 1kg</p>
-            <p class="preco verde">R$ 94,90 no PIX</p>
+            <p class="preco verde">R$ 189,90 no PIX</p>
             <button>
                <a class="btn-comprar"
-       href="../paginadoproduto.html?nome=Creatina creapure 1kgg&preco=94.90&img=imgcreatina/creapure1kg.jpeg&desc=Creatina creapure 1kg">
+       href="paginadoproduto.html?nome=Creatina creapure 1kg&preco=189,90&img=imgcreatina/creapure1kg.jpeg&desc=Creatina creapure 1kg">
         Comprar
     </a>
             </button>
@@ -324,64 +326,5 @@ $dados = mysqli_fetch_assoc($res);
         © 2026 Nutri&Treino — Todos os direitos reservados.
     </div>
 </footer>
-
-<!-- ===== PAINEL LATERAL ===== -->
-   <form id="painel" class="painel" method="POST" action="../t_usuario/salvar_perfil.php">
-
-    <button type="button" class="fechar" onclick="fecharPainel()">✖</button>
-
-    <h2>Perfil</h2>
-
-    <p class="nome-usuario">
-    <?php echo $_SESSION["usuario_nome"]; ?>
-</p>
-
-    <div class="item">
-        <label>Genero</label>
-        <select name="genero" required>
-            <option value="Masculino"
-              <?php if(($dados['genero'] ?? '') == "Masculino") echo "selected"; ?>>
-              Masculino
-            </option>
-            <option value="Feminino"<?php if(($dados['genero'] ?? '') == "Feminino") echo "selected"; ?>>
-              Feminino
-            </option>
-        </select>
-    </div>
-
-    <div class="item">
-        <label>Altura</label>
-        <input type="text" name="altura" value="<?php echo $dados['altura'] ?? ''; ?>" placeholder="Ex: 1.75" required>
-    </div>
-
-    <div class="item">
-        <label>Idade</label>
-        <input type="number" name="idade" value="<?php echo $dados['idade'] ?? ''; ?>" required>
-    </div>
-
-    <div class="item">
-        <label>Peso</label>
-        <input type="number" name="peso" value="<?php echo $dados['peso'] ?? ''; ?>" required>
-    </div>
-
-    <button type="submit" class="btn-salvar">Salvar</button>
-
-    <button type="button" class="btn-sair" onclick="window.location.href='logout.php'">
-    Sair da conta
-</button>
-
-</form>
-  
-</div>
-
-<script>
-function abrirPainel() {
-    document.getElementById("painel").classList.add("ativo");
-}
-
-function fecharPainel() {
-    document.getElementById("painel").classList.remove("ativo");
-}
-</script>
-</body>
-</html>
+ </body>
+ </html>
