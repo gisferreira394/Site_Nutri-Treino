022be22fc7516bb3661e014fdf2ad120790e8832
geovana <geovanasilva394@gmail.com>
2026-04-29 14:15:14 -0300

    .

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,18 +1,9 @@
+
 <?php
-$bd_servidor = getenv("MYSQLHOST");
-$bd_usuario  = getenv("MYSQLUSER");
-$bd_senha    = getenv("MYSQLPASSWORD");
-$bd_banco    = getenv("MYSQLDATABASE");
-$bd_porta    = getenv("MYSQLPORT");
+$bd_usuario = "root";
+$bd_senha = "";
+$bd_servidor = "localhost";
+$bd_banco = "nutri-treino";
+$conexao = mysqli_connect($bd_servidor,$bd_usuario,$bd_senha,$bd_banco);
+?>
 
-$conexao = mysqli_connect(
-    $bd_servidor,
-    $bd_usuario,
-    $bd_senha,
-    $bd_banco,
-    $bd_porta
-);
-
-if (!$conexao) {
-    die("Erro ao conectar ao banco de dados");
-}
